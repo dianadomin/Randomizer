f941c94bd8e20c98eb670cbb025a550c24a3b315
dianadomin <47133313+dianadomin@users.noreply.github.com>
2021-03-15 01:14:30 -0700

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -1,11 +1,15 @@
 //dogs I  have loved before
-let dogs = ["di/lon", "Ziggy", "mercedes", "nala", "kloot", "petunia"]
+let dogs = ["dillon", "Ziggy", "mercedes", "nala", "kloot", "petunia"]
 
 function setup() {
   createCanvas(600, 600);
   background(200);
 
-  console.log(dogs[0]);
+
+dogs[1] = "ziggy startdust";
+  console.log(dogs);
+
+
 }
 
 function draw() {
