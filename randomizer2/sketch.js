6918658ded1da597da76fab3ee49cf10ef32a304
dianadomin <47133313+dianadomin@users.noreply.github.com>
2021-03-15 19:06:36 -0700

    Changed from flowers to forutne
    Decided to change my randomizer from creating a flower bouquet to a fortune teller

--- randomizer2/sketch.js
+++ randomizer2/sketch.js
@@ -1,51 +1,50 @@
-//Flowers/Bouquet
-let flowers = [{
-    name: "Rose",
-    color: "dog treat color"
-  }, {
-    name: "Iris",
-    color: "chartrese"
-  }, {
-    name: "Hydrangea",
-    color: "poop"
-  }, {
-    name: "Carnation",
-    color: "rose gold"
-  }, {
-    name: "Peony",
-    color: "coffee grind brown"
-  }, {
-    name: "Hyacinth",
-    color: "stardust"
-  }, {
-    name: "Lilac",
-    color: "lkfjs"
-  }, {
-    name: "Orchid",
-    color: "sfkhekjf"
-  }, {
-    name: "Sunflower",
-    color: "yellow"
-  }, {
-    name: "Lily",
-    color: "Pink"
-  }, {
-    name: "Tulip",
-    color: "Blue"
-  }];
+//FortuneTeller
+let fortunes = [{
+  name: "You will have a good day tomorrow",
+}, {
+  name: "A surprise encounter will happen soon",
+}, {
+  name: "Something is good coming your way",
+}, {
+  name: "You are going to pass your classes",
+}, {
+  name: "You will be hungry again in 3 hours",
+}, {
+  name: "Wear your favorite color today to avoid bad luck",
+}, {
+  name: "The pain you feel now will soon pass",
+}, {
+  name: "You will reconnect with someone soon",
+}, {
+  name: "You will get a good nights rest",
+}, {
+  name: "Treat yourself today, reap the results tomorrow",
+}, {
+  name: "Today 7 is your lucky number",
+}];
 
 let randomIndex;
 
 function setup() {
   createCanvas(600, 600);
   background(171, 144, 212);
+  textSize(32);
+
+  text("Click to get your fortune told!", 50, 50)
 
 }
 
 function draw() {}
 
+
 function mousePressed() {
-  background(random(200, 255));
-  randomIndex = int(random(flowers.length));
-  text(flowers[randomIndex].name, 50, 50);
+  if (fortunes[0]) {
+    background(random(200, 255));
+    randomIndex = int(random(fortunes.length));
+    text(fortunes[randomIndex].name, 50, 50);
+    fortunes.splice(randomIndex, 1);
+  } else {
+    background(255, 253, 217);
+    text("There are no more fortunes for The future is yours to write!", 50, 50)
+  }
 }
